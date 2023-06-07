f9d4f28a9da3ea0114406a492422afc25282e304
Pablo García de Gregorio <129775284+pablogarciadegregorio@users.noreply.github.com>
2023-06-07 10:20:33 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,46 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let number = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let suit = ["♠", "♣", "♥", "♦"];
+
+  function Random(maximo) {
+    return Math.floor(Math.random() * maximo);
+  }
+  let topRandom = suit[Random(suit.length)];
+  let midRandom = number[Random(number.length)];
+  let top = document.getElementById("top");
+
+  top.innerHTML = bot.innerHTML = topRandom;
+
+  let mid = document.getElementById("mid");
+  mid.innerHTML = midRandom;
+
+  if (topRandom === "♦" || topRandom === "♥") {
+    top = document.getElementById("top");
+    top.style.color = "red";
+    bot.style.color = "red";
+  }
+
+  // OPCIONAL
+
+  function generateNewCard() {
+    let newCard = document.querySelector("new-card");
+    newCard.style.color = "blue";
+  }
 };
